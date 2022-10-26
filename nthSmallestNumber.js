dcfc12b5823e23153f4d455003773966d1e5f81a
Eric <gentlegiantdev@gmail.com>
2022-10-26 10:16:29 -0700

    another challenge down

---
+++ nthSmallestNumber.js
@@ -0,0 +1,22 @@
+// Given an array of integers , Find the Nth smallest element in this array of integers.
+
+// Notes
+// Array/list size is at least 3 .
+
+// Array/list's numbers could be a mixture of positives , negatives and zeros .
+
+// Repetition in array/list's numbers could occur , so don't Remove Duplications .
+
+// nthSmallest({3,1,2} ,2) ==> return (2) 
+// nthSmallest({15,20,7,10,4,3} ,3) ==> return (7) 
+// nthSmallest({15,20,7,10,4,3} ,3) ==> return (7) 
+// nthSmallest({177,225,243,-169,-12,-5,2,92} ,5) ==> return (92) 
+
+//given array and n
+// return the nth smallest element in the array
+
+function nthSmallest(arr, num){
+ return arr.sort((a,b) => a - b)[num-1];
+}
+
+console.log(nthSmallest([3,1,2],2))
