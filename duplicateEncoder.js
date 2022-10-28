879645c65e928a504957d2b39e83c647c70013ae
Eric <gentlegiantdev@gmail.com>
2022-10-28 12:03:01 -0700

    another challenge down

--- duplicateEncoder.js
+++ duplicateEncoder.js
@@ -1,12 +1,12 @@
-The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
 
-Examples
-"din"      =>  "((("
-"recede"   =>  "()()()"
-"Success"  =>  ")())())"
-"(( @"     =>  "))((" 
+// Examples
+// "din"      =>  "((("
+// "recede"   =>  "()()()"
+// "Success"  =>  ")())())"
+// "(( @"     =>  "))((" 
 
-takes in a string. ignore capitalization
+// takes in a string. ignore capitalization
 
 
 // function duplicateEncoder(str){
