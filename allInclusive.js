0b6e62e3440fcc586c1f66cf7d11b42765b9d76d
Eric <gentlegiantdev@gmail.com>
2022-11-12 14:43:56 -0700

    another challenge down

---
+++ allInclusive.js
@@ -0,0 +1,22 @@
+// Input:
+
+// a string strng
+// an array of strings arr
+// Output of function contain_all_rots(strng, arr) (or containAllRots or contain-all-rots):
+
+// a boolean true if all rotations of strng are included in arr
+// false otherwise
+// Examples:
+// contain_all_rots(
+//   "bsjq", ["bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs"]) -> true
+
+// contain_all_rots(
+//   "Ajylvpy", ["Ajylvpy", "ylvpyAj", "jylvpyA", "lvpyAjy", "pyAjylv", "vpyAjyl", "ipywee"]) -> false)
+
+ function containAllRots(strng, arr) {
+    for(i = 0; i < strng.length; i++){
+        if(arr.indexOf(strng.slice(i) + strng.slice(0,i)) == -1)
+        return false;
+    }
+    return true;
+}
