f944548245a40addcd3ed3b622dcec53b6dae90e
Eric <gentlegiantdev@gmail.com>
2022-11-14 13:56:28 -0700

    another challenge down

---
+++ middleElement.js
@@ -0,0 +1,27 @@
+// You need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+
+// The input to the function will be an array of three distinct numbers.
+
+// For example:
+
+// gimme([2, 3, 1]) => 0
+// 2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+// Another example (just to make sure it is clear):
+
+// gimme([5, 10, 14]) => 1
+// 10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
+
+//sort
+//pull middle number
+//find middle number in original arr
+//return its index
+
+function gimme (triplet) {
+  let sortedArr = triplet.slice().sort((a,b) => a - b);
+  return triplet.indexOf(sortedArr[1])
+}
+
+// function gimme(a) {
+//   return a.indexOf(a.concat().sort(function(a, b) { return a - b })[1])
+// }
