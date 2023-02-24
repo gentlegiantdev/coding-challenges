406bfb3cf6eff9206805a2b1a966672d59c1d947
Eric <gentlegiantdev@gmail.com>
2023-02-24 11:52:48 -0700

    my brain is still functioning

---
+++ doubleEveryOther.js
@@ -0,0 +1,22 @@
+// Write a function that doubles every second integer in a list, starting from the left.
+
+// Example:
+
+// For input array/list :
+
+// [1,2,3,4]
+// the function should return :
+
+// [1,4,3,8]
+
+
+function doubleEveryOther(a) {
+  for(i=0;i<a.length;i++){
+    if(i % 2 != 0){
+      a[i] = a[i] * 2 
+    }
+  }
+  return a;
+}
+
+
